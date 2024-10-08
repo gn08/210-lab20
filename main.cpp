e0c87db71771f98faff99acdb58c88d73c24206e
gn08 <gabbyfilipova@gmail.com>
2024-10-08 15:43:16 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -5,21 +5,27 @@
 
 using namespace std;
 const int SIZE = 3;
+//constant for random prices
 const int MIN = 10000, MAX = 99999;
 
 class Chair {
 private:
+//legs on chair
     int legs;
+//price history
     double * prices;
 public:
     // constructors
     Chair() {
+    //random number of legs, 3 or 4, random prices
         prices = new double[SIZE];
         legs = rand() % 2 + 3;
         for (int i = 0; i < SIZE; i++){
             prices[i] = (rand() % (MAX - MIN + 1) + MIN) / 100.0;
         }
     }
+//set to specifc nuber of legs and price history
+//arguments: l- legs, p[SIZE]- prices
     Chair(int l, double p[SIZE]) {
         prices = new double[SIZE];
         legs = l;
@@ -29,13 +35,19 @@ public:
     }
 
     // setters and getters
+    //set number legs on chair
+    //arguments: l- number of legs
     void setLegs(int l)      { legs = l; }
+    //get number of legs o chair
+    //return: number of legs
     int getLegs()            { return legs; }
-
+    //set prices for price history
+    //arguemnts: p1, p2, p3- price values
     void setPrices(double p1, double p2, double p3) { 
         prices[0] = p1; prices[1] = p2; prices[2] = p3; 
     }
-
+    //get average price history
+    //return: average price
     double getAveragePrices() {
         double sum = 0;
         for (int i = 0; i < SIZE; i++){
@@ -43,7 +55,7 @@ public:
         }
         return sum / SIZE;
     }
-
+//print info of chair
     void print() {
         cout << "CHAIR DATA - legs: " << legs << endl;
         cout << "Price history: " ;
@@ -53,13 +65,14 @@ public:
         cout << endl << "Historical avg price: " << getAveragePrices();
         cout << endl << endl;
     }
-
+//destructor- frees up memory
     ~Chair(){
         delete[] prices;
     }
 };
 
 int main() {
+    //random generator for time
     srand(time(0));
     cout << fixed << setprecision(2);
 
